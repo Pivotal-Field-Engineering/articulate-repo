5625e6524784d2095e737a444280b1775ee73c1a
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-24 22:31:47 -0500

    install helm

--- ci/concourse/config/deploy-pks-app.sh
+++ ci/concourse/config/deploy-pks-app.sh
@@ -23,5 +23,8 @@ kubectl get pods --all-namespaces
 
 uname -a 
 
-apt-get install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 helm list
