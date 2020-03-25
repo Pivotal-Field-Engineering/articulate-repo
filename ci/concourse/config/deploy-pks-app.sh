c152244a10e0c9af750663cdb57a24d6a3b31321
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-24 22:40:19 -0500

    updated script

--- ci/concourse/config/deploy-pks-app.sh
+++ ci/concourse/config/deploy-pks-app.sh
@@ -27,4 +27,5 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-helm install ins1 pks-demo
+cd ./helm-chart
+helm upgrade ins1 pks-demo
