e40e4024380ebc1698e82e089e5c25a3a5e255eb
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-26 10:46:45 -0500

    updated scan

--- ci/concourse/tasks/scan/scan.sh
+++ ci/concourse/tasks/scan/scan.sh
@@ -3,9 +3,12 @@
 set -eu
 uname -a
 
+echo https://${HARBOR_HOST}/api/repositories/caas-workshop/mysql/tags/${TAG_NAME}
+
 #wget -O /tmp/scan.json --no-check-certificate  --header "Authorization: Basic ${HARBOR_PASSWORD}" --header "Content-Type: application/json" https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/1.16.289
 wget -O /tmp/scan.json --no-check-certificate  --header "Authorization: Basic ${HARBOR_PASSWORD}" --header "Content-Type: application/json" https://${HARBOR_HOST}/api/repositories/caas-workshop/mysql/tags/${TAG_NAME}
 
+https://${HARBOR_HOST}/api/repositories/caas-workshop/mysql/tags/${TAG_NAME}
 
 Result=`cat /tmp/scan.json | yq r - "scan_overview.*.severity"`
 
