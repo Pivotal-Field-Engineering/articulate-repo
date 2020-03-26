41cea143ba9a86e951ed1f141a5c5354013f0b14
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-26 01:22:49 -0500

    updated test task

--- ci/concourse/tasks/test/test.sh
+++ ci/concourse/tasks/test/test.sh
@@ -5,9 +5,9 @@ uname -a
 
 apt-get update
 apt-get install wget -y
-apt-get install jq -y
+apt-get install yq -y
 
 wget -O /tmp/scan.json --user ${HARBOR_USERNAME} --password ${HARBOR_PASSWORD} --no-check-certificate  --header "Content-Type: application/json" -X GET https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/1.16.289
 cat /tmp/scan.json
-apt-get install yq
+
 cat /tmp/scan.json | yq r - "scan_overview.*.severity"
