2dbf92281da4cb70d620053cd0db6e511c504a7f
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-26 10:02:11 -0500

    test.sh

--- ci/concourse/tasks/test/test.sh
+++ ci/concourse/tasks/test/test.sh
@@ -3,7 +3,9 @@
 set -eu
 uname -a
 
-wget -O /tmp/scan.json --no-check-certificate  --header "Authorization: Basic ${HARBOR_PASSWORD}" --header "Content-Type: application/json" https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/1.16.289
+#wget -O /tmp/scan.json --no-check-certificate  --header "Authorization: Basic ${HARBOR_PASSWORD}" --header "Content-Type: application/json" https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/1.16.289
+wget -O /tmp/scan.json --no-check-certificate  --header "Authorization: Basic ${HARBOR_PASSWORD}" --header "Content-Type: application/json" https://${HARBOR_HOST}/api/repositories/caas-workshop/mysql:latest
+
 
 Result=`cat /tmp/scan.json | yq r - "scan_overview.*.severity"`
 
