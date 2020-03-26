a2f1de1fbb95ab396ee764d55433fcb90df81535
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-26 01:15:58 -0500

    updated task test

--- ci/concourse/tasks/test/test.sh
+++ ci/concourse/tasks/test/test.sh
@@ -3,6 +3,7 @@
 set -eu
 uname -a
 
-curl -u ${HARBOR_USERNAME}:${HARBOR_PASSWORD} -k  --header "Content-Type: application/json" -X GET https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/1.16.289 > /tmp/apple.json
+wget -O /tmp/scan.json --user ${HARBOR_USERNAME} --password ${HARBOR_PASSWORD} --no-check-certificate  --header "Content-Type: application/json" -X GET https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/1.16.289
 
-cat /tmp/apple.json | yq r - "scan_overview.*.severity"
+apt-get install yq
+cat /tmp/scan.json | yq r - "scan_overview.*.severity"
