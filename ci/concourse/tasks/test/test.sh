61528d062e361f0fb32f69c6085f0df7074924b3
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-26 01:09:33 -0500

    task change

--- ci/concourse/tasks/test/test.sh
+++ ci/concourse/tasks/test/test.sh
@@ -2,7 +2,6 @@
 
 set -eu
 uname -a
-ls -lRta
 
 curl -u ${HARBOR_USERNAME}:${HARBOR_PASSWORD} -k  --header "Content-Type: application/json" -X GET https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/1.16.289 > /tmp/apple.json
 
