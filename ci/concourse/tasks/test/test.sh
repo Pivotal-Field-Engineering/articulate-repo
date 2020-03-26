7aa105c1fe719de61e7af8f6aa76708d36796505
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-26 01:01:01 -0500

    done

--- ci/concourse/tasks/test/test.sh
+++ ci/concourse/tasks/test/test.sh
@@ -4,6 +4,6 @@ set -eu
 uname -a
 ls -lRta
 
-curl -u ${harbor.username}:${harbor.password} -k  --header "Content-Type: application/json" -X GET "https://${harbor.host}/api/repositories/hemanth/pks-demo/tags/1.16.289" > /tmp/apple.json
+curl -u ${harbor.username}:${harbor.password} -k  --header "Content-Type: application/json" -X GET https://${harbor.host}/api/repositories/hemanth/pks-demo/tags/1.16.289 > /tmp/apple.json
 
 cat /tmp/apple.json | yq r - "scan_overview.*.severity"
