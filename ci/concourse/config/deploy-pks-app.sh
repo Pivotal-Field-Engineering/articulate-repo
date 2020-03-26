b5bf2f533aba47f5fd8c76b59dd33273712977db
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-26 17:02:13 -0500

    Updated Deploy app  script

--- ci/concourse/config/deploy-pks-app.sh
+++ ci/concourse/config/deploy-pks-app.sh
@@ -16,13 +16,6 @@ kubectl cluster-info
 
 echo "Deploying Application..."
 
-echo "Inspect ingress services and pods..."
-kubectl get services --all-namespaces
-kubectl get pods --all-namespaces
-
-
-uname -a 
-
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
