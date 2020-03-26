0bfe36b5b6655abb4d63cf6c62df2398f8feaff4
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-26 09:57:53 -0500

    updated test script

--- ci/concourse/tasks/test/test.sh
+++ ci/concourse/tasks/test/test.sh
@@ -5,6 +5,18 @@ uname -a
 
 wget -O /tmp/scan.json --no-check-certificate  --header "Authorization: Basic ${HARBOR_PASSWORD}" --header "Content-Type: application/json" https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/1.16.289
 
-cat /tmp/scan.json
+Result=`cat /tmp/scan.json | yq r - "scan_overview.*.severity"`
 
-cat /tmp/scan.json | yq r - "scan_overview.*.severity"
+if [ "$Result" != "High" ] ; then
+    echo "Container Images don't have vulnerability of defined High threshold"
+else
+    echo "Container Images has vulnerability of severity of High and above. Failing the step"
+    exit 1
+fi
+
+if [ "$Result" != "Critical" ] ; then
+    echo "Container Images don't have vulnerability of Critical threshold. Passing the step."
+else
+    echo "Container Images has vulnerability of severity of Critical and above. Failing the step"
+    exit 1
+fi
